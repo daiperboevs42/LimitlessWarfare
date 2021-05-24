b1abe9148f50fe580158db5c57cf41bd48d702b5
Chililove <mega_lulu_kryster@hotmail.com>
2021-05-24 14:58:06 +0200

    canvascreate

--- Assets/MS/M4A3E2/Vehicles/Asset/Tank Prefabs/Scripts for the tank/DoneScripts/GameManagement.cs
+++ Assets/MS/M4A3E2/Vehicles/Asset/Tank Prefabs/Scripts for the tank/DoneScripts/GameManagement.cs
@@ -7,12 +7,17 @@ public class GameManagement : MonoBehaviour
     List<GameObject> listOfEnemies = new List<GameObject>();
     private bool allEnemiesAreDead = false;
     private bool isPlayerDead = false;
+    public bool lastLevel = false;
+    public GameObject winCanvas;
+    public GameObject restartCanvas;
+    public GameObject winWinCanvas;
 
     // Start is called before the first frame update
     void Start()
     {
         listOfEnemies.AddRange(GameObject.FindGameObjectsWithTag("Enemy"));
         print(listOfEnemies.Count);
+        
     }
 
     public void KilledEnemy(GameObject enemy)
@@ -46,13 +51,20 @@ public class GameManagement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (allEnemiesAreDead)
+        if (allEnemiesAreDead && !lastLevel)
         {
+            
             //load win screen
+            winCanvas.gameObject.SetActive(true);
         }
         if (isPlayerDead)
         {
             //load death screen
+            restartCanvas.gameObject.SetActive(true);
+        }
+        if (allEnemiesAreDead && lastLevel)
+        {
+            winWinCanvas.gameObject.SetActive(true);
         }
     }
 }
