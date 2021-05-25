3276fcc5bed4c8ed4f673a4d7377f08d76a5f13d
Martin Brodersen <martinroager@gmail.com>
2021-05-25 23:37:02 +0200

    Several fixes
    added proper camera, tweaked the tanks, fixed the light, tweaked the hierachy.

---
+++ Assets/Tanks-MS/Assets/Scripts for the tank/DoneScripts/CameraFollow.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject player;
+    public float cameraHeight = 30.0f;
+
+    void Update()
+    {
+        Vector3 pos = player.transform.position;
+        pos.y += cameraHeight;
+        transform.position = pos;
+    }
+}
